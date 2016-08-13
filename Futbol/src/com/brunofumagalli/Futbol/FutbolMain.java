854c8c668d0191cdfd3180451de87e10dbd9cb34
Bruno <Bruno@Simon>
2016-08-13 01:52:43 -0300

    Agregamos una clase FutbolMain

---
+++ Futbol/src/com/brunofumagalli/Futbol/FutbolMain.java
@@ -0,0 +1,11 @@
+package com.brunofumagalli.Futbol;
+
+public class FutbolMain {
+
+	public static void main(String[] args) {
+	
+		Jugador jug = new Jugador("Bruno");
+	    String nombreDelJugador = jug.getName(); 
+	}
+
+}
--- Futbol/src/com/brunofumagalli/Futbol/Jugador.java
+++ Futbol/src/com/brunofumagalli/Futbol/Jugador.java
@@ -2,10 +2,19 @@ package com.brunofumagalli.Futbol;
 
 public class Jugador {
 
-  private String name;
+	private String name;
 
-  public Jugador(String name) {
-    this.name = name;
-  }
+	public Jugador(String name) {
+		this.name = name;
 
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
 }
+
