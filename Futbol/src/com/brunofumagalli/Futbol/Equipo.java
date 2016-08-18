403cd07e974925320c635bda5c72756a705536cd
Bruno <Bruno@Simon>
2016-08-18 02:29:56 -0300

    Clase equipo

---
+++ Futbol/src/com/brunofumagalli/Futbol/Equipo.java
@@ -0,0 +1,20 @@
+package com.brunofumagalli.Futbol;
+
+public class Equipo {
+	private String equipo_1;
+	private String equipo_2;
+	
+	
+	public Equipo(String equipo_1, String equipo_2, Jugador jugador) {
+		this.equipo_1 = equipo_1;
+		this.equipo_2 = equipo_2;
+		
+				
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
